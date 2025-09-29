84030ba8d12fba23254c1bbc965182f49b58b72b
swooshi-sushii <sadie3868@gmail.com>
2025-09-29 12:41:56 -0400

    Button Changes

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const startBtn = document.getElementById("startBtn");
 const message = document.getElementById("message");
+const settingsBtn = document.getElementById("settingsBtn");
+const infoBtn = document.getElementById("infoBtn");
+
 
 startBtn.addEventListener("click", () => {
   // Change the message
   message.textContent = "The game has started!";
 
   // Change the button color
-  startBtn.style.backgroundColor = "red";
+  startBtn.style.backgroundColor = "green";
+})
+
+settingsBtn.addEventListener("click", () => {
+  message.textContent = "Settings Entered";
+
+  settingsBtn.style.backgroundColor = "orange";
+})
+
+infoBtn.addEventListener("click", () => {
+message.textContent = "Information Entered";
+
+infoBtn.style.backgroundColor = "aqua";
 });
